cc35f1aadfe4810477df609262c484280998f6a0
ritisha032 <ritishasingh74@gmail.com>
2023-06-28 09:49:54 +0530

    .

---
+++ src/components/Spinner.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Spinner.css"
+const Spinner=() => {
+    return(<div className="flex flex-col items-center space-y-2 custom-loader">
+            
+        </div>)
+}
+export default Spinner
